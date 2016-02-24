78bae9ea5bb23598ea0518823f14f9a37ca3664f
njzk2 <masperos+github@gmail.com>
2016-02-23 20:17:41 -0500

    Menu always collapsed

--- js/main.js
+++ js/main.js
@@ -12,14 +12,6 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', currentWidth);
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
     }
-
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
   });
 
   $('.btn-mobile-menu').click(function () {
