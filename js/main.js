648c00cf6d90e1dfb148f298f64afb66b9e35751
njzk2 <masperos+github@gmail.com>
2016-02-23 20:17:21 -0500

    Clean js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,15 @@ layout: null
 ---
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.content-wrapper').addClass('animated slideInRight');
     } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').css('max-width', currentWidth);
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
     }
-  })
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
@@ -21,15 +20,16 @@ $(document).ready(function () {
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
+  });
 
   $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
 
   $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+    $('.navigation-wrapper').toggleClass('visible');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
 
 })
